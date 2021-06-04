0dd0e6c407d05328176fbd4fe9da6b8f851f7b2f
Tamas K. Papp <tkpapp@gmail.com>
2021-06-04 16:11:25 +0200

    add partial for Ehat, tests
    incidental: tolerance parameter, fix broadcasting typo

--- src/NonhomotheticCES.jl
+++ src/NonhomotheticCES.jl
@@ -59,13 +59,18 @@ function bisection(f, a::T, fa::T, b::T, fb::T, xtol, max_iterations) where {T}
     error("too many iterations")
 end
 
-function calculate_Ĉ(Ê, σ, Ω̂s, ϵs, p̂s)
-    f(Ĉ) = logsumexp((Ĉ .* ϵs .+ p̂s) .* (1 - σ) + Ω̂s) / (1 - σ) - Ê
+function calculate_Ĉ(Ê, σ, Ω̂s, ϵs, p̂s; Ĉtol = 0x1p-10)
+    f(Ĉ) = logsumexp((Ĉ .* ϵs .+ p̂s) .* (1 - σ) .+ Ω̂s) / (1 - σ) - Ê
     Ĉ0 = (Ê - mean(Ω̂s) / (1 - σ) - mean(p̂s)) / mean(ϵs)
     Ĉ1, fĈ1, Ĉ2, fĈ2 = bracket_increasing(f, Ĉ0, 1.0, 2.0, 50)
-    Ĉ, r = bisection(f, Ĉ1, fĈ1, Ĉ2, fĈ2, 0x1p-10, 30)
+    Ĉ, r = bisection(f, Ĉ1, fĈ1, Ĉ2, fĈ2, Ĉtol, 50)
     # FIXME check residual r?
     Ĉ
+end
+
+function ∂Ĉ∂Ê(Ĉ, Ê, σ, Ω̂s, ϵs, p̂s)
+    # FIXME reuse common terms in exponential
+    exp((1 - σ) * Ê - logsumexp((Ĉ .* ϵs .+ p̂s) .* (1 - σ) .+ Ω̂s .+ log.(ϵs)))
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using NonhomotheticCES
-using NonhomotheticCES: calculate_Ĉ
-using LogExpFunctions, Test, StaticArrays, UnPack
+using NonhomotheticCES: calculate_Ĉ, ∂Ĉ∂Ê
+using FiniteDifferences, LogExpFunctions, Test, StaticArrays, UnPack
 
 function random_parameters(::Val{N}) where N
     (Ê = rand() * 5.0,
@@ -10,10 +10,24 @@ function random_parameters(::Val{N}) where N
      p̂s = randn(SVector{N}))
 end
 
+function ∂(f, x; h = 0.01)
+    central_fdm(20, 1)(f, x, h)
+end
+
 @testset "finding Ĉ" begin
     for _ in 1:100
         @unpack Ê, σ, Ω̂s, ϵs, p̂s = random_parameters(Val(4))
         Ĉ = calculate_Ĉ(Ê, σ, Ω̂s, ϵs, p̂s)
         @test logsumexp((Ĉ .* ϵs .+ p̂s) .* (1-σ) .+ Ω̂s)/(1-σ) ≈ Ê atol = 1e-2
+    end
+end
+
+@testset "partials" begin
+    Ĉtol = 0x1p-20
+    for _ in 1:100
+        @unpack Ê, σ, Ω̂s, ϵs, p̂s = random_parameters(Val(2))
+        Ĉ = calculate_Ĉ(Ê, σ, Ω̂s, ϵs, p̂s; Ĉtol = Ĉtol)
+        @test ∂Ĉ∂Ê(Ĉ, Ê, σ, Ω̂s, ϵs, p̂s) ≈
+            ∂(e -> calculate_Ĉ(e, σ, Ω̂s, ϵs, p̂s; Ĉtol = Ĉtol), Ê) atol = 1e-4 rtol = 1e-4
     end
 end
