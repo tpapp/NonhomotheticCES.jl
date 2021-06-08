d57844439ec9b613b85edc49ce4db7a1ea166983
Tamas K. Papp <tkpapp@gmail.com>
2021-06-08 16:07:24 +0200

    add partials for all parameters (#5)
    * add partial for Ehat, tests
    
    incidental: tolerance parameter, fix broadcasting typo
    
    * test differently
    
    * more derivatives, more sophisticated testing
    
    * more argcheck
    
    * derivatives in sigma
    
    * reuse sum

--- src/NonhomotheticCES.jl
+++ src/NonhomotheticCES.jl
@@ -59,13 +59,42 @@ function bisection(f, a::T, fa::T, b::T, fb::T, xtol, max_iterations) where {T}
     error("too many iterations")
 end
 
-function calculate_Ĉ(Ê, σ, Ω̂s, ϵs, p̂s)
-    f(Ĉ) = logsumexp((Ĉ .* ϵs .+ p̂s) .* (1 - σ) + Ω̂s) / (1 - σ) - Ê
+function calculate_Ĉ(Ê, σ, Ω̂s, ϵs, p̂s; Ĉtol = 0x1p-10, skipcheck::Bool = false)
+    if !skipcheck
+        @argcheck σ > 0 && σ ≠ 1 && all(ϵs .> 0)
+    end
+    f(Ĉ) = logsumexp((Ĉ .* ϵs .+ p̂s) .* (1 - σ) .+ Ω̂s) / (1 - σ) - Ê
     Ĉ0 = (Ê - mean(Ω̂s) / (1 - σ) - mean(p̂s)) / mean(ϵs)
     Ĉ1, fĈ1, Ĉ2, fĈ2 = bracket_increasing(f, Ĉ0, 1.0, 2.0, 50)
-    Ĉ, r = bisection(f, Ĉ1, fĈ1, Ĉ2, fĈ2, 0x1p-10, 30)
+    Ĉ, r = bisection(f, Ĉ1, fĈ1, Ĉ2, fĈ2, Ĉtol, 50)
     # FIXME check residual r?
     Ĉ
 end
+
+"""
+$(SIGNATURES)
+
+Calculate and return
+
+1. `Zs`, where each element is ``Zᵢ ∝ exp(zᵢ)``, scaled to protect from under- and overflow,
+where ``zᵢ = (Ĉ ϵᵢ + pᵢ) (1 - σ) + Ωᵢ``,
+
+2. `sum(Zs .* ϵs)`.
+"""
+function calculate_Zs_∑Zϵ(Ĉ, Ê, σ, Ω̂s, ϵs, p̂s)
+    zs = (Ĉ .* ϵs .+ p̂s) .* (1 - σ) .+ Ω̂s
+    Zs = exp.(zs .- maximum(zs))
+    Zs, sum(Zs .* ϵs)
+end
+
+calculate_∂Ê(Zs, ∑Zϵ) = sum(Zs) / ∑Zϵ
+
+calculate_∂p̂s(Zs, ∑Zϵ) = Zs ./ -∑Zϵ
+
+calculate_∂ϵs(∂p̂s, Ĉ) = Ĉ .* ∂p̂s
+
+calculate_∂Ω̂s(∂p̂s, σ) = ∂p̂s ./ (1 - σ)
+
+calculate_∂σ(Zs, ∑Zϵ, Ĉ, Ê, σ, ϵs, p̂s) = (Ĉ + sum(Zs .* (p̂s .- Ê)) / ∑Zϵ) / (1 - σ)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,29 @@
 using NonhomotheticCES
-using NonhomotheticCES: calculate_Ĉ
-using LogExpFunctions, Test, StaticArrays, UnPack
+using NonhomotheticCES:         # internals
+    calculate_Ĉ, calculate_Zs_∑Zϵ, calculate_∂Ê, calculate_∂p̂s, calculate_∂ϵs,
+    calculate_∂Ω̂s, calculate_∂σ
 
-function random_parameters(::Val{N}) where N
+using FiniteDifferences, LogExpFunctions, Test, StaticArrays, UnPack
+
+"""
+Random parameters with `N` sectors. `L` is added to positive parameters, to bound away from
+`0`.
+"""
+function random_parameters(::Val{N}, L = 0.1) where N
     (Ê = rand() * 5.0,
-     σ = rand() * 2.0,
+     σ = rand() * 2.0 .+ L,
      Ω̂s = randn(SVector{N}),
-     ϵs = rand(SVector{N}) .* 3.0,
+     ϵs = rand(SVector{N}) .* 2.0 .+ L,
      p̂s = randn(SVector{N}))
+end
+
+∂(f; x = 0.0, kwargs...) = central_fdm(5, 1; factor = 1e6, kwargs...)(f, x)
+
+"Add `x` at `i`."
+function add_at(v::SVector, i::Int, x)
+    m = MVector(v)
+    m[i] += x
+    SVector(m)
 end
 
 @testset "finding Ĉ" begin
@@ -15,5 +31,38 @@ end
         @unpack Ê, σ, Ω̂s, ϵs, p̂s = random_parameters(Val(4))
         Ĉ = calculate_Ĉ(Ê, σ, Ω̂s, ϵs, p̂s)
         @test logsumexp((Ĉ .* ϵs .+ p̂s) .* (1-σ) .+ Ω̂s)/(1-σ) ≈ Ê atol = 1e-2
+    end
+end
+
+@testset "partials building blocks" begin
+    Ĉtol = 0x1p-20
+    atol = 1e-2
+    rtol = 1e-2
+    for _ in 1:100
+        N = Val{2}()
+        @unpack Ê, σ, Ω̂s, ϵs, p̂s = random_parameters(N)
+        Ĉ = calculate_Ĉ(Ê, σ, Ω̂s, ϵs, p̂s; Ĉtol = Ĉtol)
+        Zs, ∑Zϵ = calculate_Zs_∑Zϵ(Ĉ, Ê, σ, Ω̂s, ϵs, p̂s)
+        ∂p̂s = calculate_∂p̂s(Zs, ∑Zϵ)
+        ∂ϵs = calculate_∂ϵs(∂p̂s, Ĉ)
+        ∂Ω̂s = calculate_∂Ω̂s(∂p̂s, σ)
+        @test calculate_∂Ê(Zs, ∑Zϵ) ≈
+            ∂(h -> calculate_Ĉ(Ê + h, σ, Ω̂s, ϵs, p̂s; Ĉtol = Ĉtol)) atol = atol rtol = rtol
+        for (j, ∂p̂) in pairs(∂p̂s)
+            ∂p̂_fd = ∂(h -> calculate_Ĉ(Ê, σ, Ω̂s, ϵs, add_at(p̂s, j, h); Ĉtol = Ĉtol))
+            @test ∂p̂ ≈ ∂p̂_fd atol = atol rtol = rtol
+        end
+        for (j, ∂ϵ) in pairs(∂ϵs)
+            ∂ϵ_fd = ∂(h -> calculate_Ĉ(Ê, σ, Ω̂s, add_at(ϵs, j, h), p̂s; Ĉtol = Ĉtol);
+                      max_range = ϵs[j] * 0.99)
+            @test ∂ϵ ≈ ∂ϵ_fd atol = atol rtol = rtol
+        end
+        for (j, ∂Ω̂) in pairs(∂Ω̂s)
+            ∂Ω̂_fd = ∂(h -> calculate_Ĉ(Ê, σ, add_at(Ω̂s, j, h), ϵs, p̂s; Ĉtol = Ĉtol))
+            @test ∂Ω̂ ≈ ∂Ω̂_fd atol = atol rtol = rtol
+        end
+        @test calculate_∂σ(Zs, ∑Zϵ, Ĉ, Ê, σ, ϵs, p̂s) ≈
+            ∂(h -> calculate_Ĉ(Ê, σ + h, Ω̂s, ϵs, p̂s; Ĉtol = Ĉtol);
+              max_range = σ * 0.99) atol = atol rtol = rtol
     end
 end
