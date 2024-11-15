b076d99ce3298c5590e76ea570d94f89dcfe11e1
Tamás K. Papp <tkpapp@gmail.com>
2024-11-15 17:37:12 +0100

    use extensions instead of Requires

--- src/NonhomotheticCES.jl
+++ src/NonhomotheticCES.jl
@@ -9,7 +9,6 @@ using ArgCheck: @argcheck
 using DocStringExtensions: SIGNATURES
 using LinearAlgebra: dot
 using LogExpFunctions: logsumexp
-using Requires: @require
 using StaticArrays: SVector
 using Statistics: mean
 
@@ -80,31 +79,6 @@ function log_consumption_aggregator(U::NonhomotheticCESUtility{N,Tσ,TΩ̂,Tϵ},
     (; σ, Ω̂s, ϵs) = U
     @argcheck isfinite(Ê) && all(isfinite, p̂s) DomainError
     calculate_Ĉ(Ê, σ, Ω̂s, ϵs, p̂s; tol = tol, skipcheck = true)
-end
-
-function __init__()
-    @require ForwardDiff="f6369f11-7733-5829-9624-2563aa707210" begin
-        using .ForwardDiff: Dual, value, partials, valtype
-        import NonhomotheticCES: calculate_Ĉ
-
-        partials_product(x, y, α = 1) = mapreduce((x, y) -> partials(x) * y * α, +, x, y)
-
-        function calculate_Ĉ(::Type{T}, Ê, σ, Ω̂s, ϵs, p̂s;
-                             tol = DEFAULT_TOL, skipcheck = false) where {T<:Dual}
-            vÊ, vσ, vΩ̂s, vϵs, vp̂s = value(Ê), value(σ), value.(Ω̂s), value.(ϵs), value.(p̂s)
-            Ĉ = calculate_Ĉ(valtype(T), vÊ, vσ, vΩ̂s, vϵs, vp̂s;
-                            tol = tol, skipcheck = skipcheck)
-            Zs, ∑Zϵ = calculate_Zs_∑Zϵ(Ĉ, vÊ, vσ, vΩ̂s, vϵs, vp̂s)
-            # NOTE: we could be clever here and only calculate what is actually needed
-            ∂p̂s = calculate_∂p̂s(Zs, ∑Zϵ)
-            dĈ = (partials(Ê) * calculate_∂Ê(Zs, ∑Zϵ) +                      # ∂Ê
-                  partials(σ) * calculate_∂σ(Zs, ∑Zϵ, Ĉ, vÊ, vσ, vϵs, vp̂s) + # ∂σ
-                  partials_product(Ω̂s, ∂p̂s, inv(1 - vσ)) +                   # ∂Ω̂
-                  partials_product(ϵs, ∂p̂s, Ĉ) +                             # ∂ϵs
-                  partials_product(p̂s, ∂p̂s))                                 # ∂p̂s
-            T(Ĉ, dĈ)
-        end
-    end
 end
 
 """
