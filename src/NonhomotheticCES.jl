aa5ef0f81b88820cc396601f90a7cd276ca61245
Tamas K. Papp <tkpapp@gmail.com>
2024-11-15 17:44:38 +0100

    depend on Julia 1.10, update actions, use extensions (#24)
    * depend on Julia 1.10, update actions
    
    * minor cleanup
    
    * remove UnPack
    
    * use extensions instead of Requires
    
    * version bump
    
    * commit forgotten file, oops

--- src/NonhomotheticCES.jl
+++ src/NonhomotheticCES.jl
@@ -9,10 +9,8 @@ using ArgCheck: @argcheck
 using DocStringExtensions: SIGNATURES
 using LinearAlgebra: dot
 using LogExpFunctions: logsumexp
-using Requires: @require
 using StaticArrays: SVector
 using Statistics: mean
-using UnPack: @unpack
 
 include("internals.jl")
 
@@ -78,34 +76,9 @@ prices `p̂s` and **log** expenditure `Ê`; within (absolute) tolerance `tol`.
 function log_consumption_aggregator(U::NonhomotheticCESUtility{N,Tσ,TΩ̂,Tϵ},
                                     p̂s::SVector{N,Tp̂}, Ê::TÊ;
                                     tol = DEFAULT_TOL) where {N,Tσ,TΩ̂,Tϵ,TÊ,Tp̂}
-    @unpack σ, Ω̂s, ϵs = U
+    (; σ, Ω̂s, ϵs) = U
     @argcheck isfinite(Ê) && all(isfinite, p̂s) DomainError
     calculate_Ĉ(Ê, σ, Ω̂s, ϵs, p̂s; tol = tol, skipcheck = true)
-end
-
-function __init__()
-    @require ForwardDiff="f6369f11-7733-5829-9624-2563aa707210" begin
-        using .ForwardDiff: Dual, value, partials, valtype
-        import NonhomotheticCES: calculate_Ĉ
-
-        partials_product(x, y, α = 1) = mapreduce((x, y) -> partials(x) * y * α, +, x, y)
-
-        function calculate_Ĉ(::Type{T}, Ê, σ, Ω̂s, ϵs, p̂s;
-                             tol = DEFAULT_TOL, skipcheck = false) where {T<:Dual}
-            vÊ, vσ, vΩ̂s, vϵs, vp̂s = value(Ê), value(σ), value.(Ω̂s), value.(ϵs), value.(p̂s)
-            Ĉ = calculate_Ĉ(valtype(T), vÊ, vσ, vΩ̂s, vϵs, vp̂s;
-                            tol = tol, skipcheck = skipcheck)
-            Zs, ∑Zϵ = calculate_Zs_∑Zϵ(Ĉ, vÊ, vσ, vΩ̂s, vϵs, vp̂s)
-            # NOTE: we could be clever here and only calculate what is actually needed
-            ∂p̂s = calculate_∂p̂s(Zs, ∑Zϵ)
-            dĈ = (partials(Ê) * calculate_∂Ê(Zs, ∑Zϵ) +                      # ∂Ê
-                  partials(σ) * calculate_∂σ(Zs, ∑Zϵ, Ĉ, vÊ, vσ, vϵs, vp̂s) + # ∂σ
-                  partials_product(Ω̂s, ∂p̂s, inv(1 - vσ)) +                   # ∂Ω̂
-                  partials_product(ϵs, ∂p̂s, Ĉ) +                             # ∂ϵs
-                  partials_product(p̂s, ∂p̂s))                                 # ∂p̂s
-            T(Ĉ, dĈ)
-        end
-    end
 end
 
 """
@@ -120,7 +93,7 @@ logsumexp(log_sectoral_consumptions(U, p̂s, Ê, Ĉ) .+ p̂s) ≈ Ê
 ```
 """
 function log_sectoral_consumptions(U::NonhomotheticCESUtility, p̂s, Ê, Ĉ)
-    @unpack σ, Ω̂s, ϵs = U
+    (; σ, Ω̂s, ϵs) = U
     Ω̂s .- σ .* (p̂s .- Ê) + ((1 - σ) * Ĉ) .* ϵs
 end
 
